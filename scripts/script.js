ca52243d64122ce2a68ac9182c076c227e7b0104
KRILLE12345 <96288433+KRILLE12345@users.noreply.github.com>
2022-05-25 11:12:49 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,23 @@ for(let i = 0; i < items.length; i++) {
         console.log(i)
     })
 }
+
+let generate = document.querySelector("#generate")
+
+generate.addEventListener("click", function() {
+    let rows = document.getElementById("rows").value
+    let columns = document.getElementById("columns").value
+    let container = document.getElementById("container")
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+
+    let toAppend = ``;
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < columns; j++) {
+
+            // toAppend += `<div class = item>${i + " " + j}</div>`
+            toAppend += `<div class = item></div>`
+        }
+    }
+    container.innerHTML = toAppend;
+})
